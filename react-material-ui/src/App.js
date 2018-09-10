49d09ebe0dacb9184f1d4647e42fafcb1a82a3f7
christos.dimizas <christos.dimizas@marinetraffic.com>
2018-09-10 14:51:20 +0300

    Add material ui tabs

--- react-material-ui/src/App.js
+++ react-material-ui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import Grid from './components/Grid';
 import GridList from './components/GridList';
+import Tabs from './components/Tabs';
 class App extends Component {
     render() {
         return (
@@ -9,6 +10,7 @@ class App extends Component {
                 <NavBar />
                 <Grid />
                 <GridList />
+                <Tabs />
             </div>
         )
     }
